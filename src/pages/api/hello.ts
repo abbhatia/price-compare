860fde3b66ccb2aea8db0c5916ded69007413b72
Web Dev Cody <webdevcody@gmail.com>
2023-05-06 21:27:01 -0400

    add header

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
+
+let coldStart = true;
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const isCold = coldStart;
+  coldStart = false;
+  res
+    .status(200)
+    .setHeader("x-cold-start", `${isCold}`)
+    .json({ name: "John Doe" });
 }
